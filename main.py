09e3b496a24e438fb6e1b99890a19f4d0c503950
Scroogebcduck <63424192+Scroogebcduck@users.noreply.github.com>
2023-04-10 00:08:07 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -15,33 +15,33 @@ main.add('Понедельник').add('Вторник').add('Среда').add('
 
 
 @dp.message_handler(commands=['start'])
-async def cmd_start(message: types.Message):
+async def cmd_start(message:types.Message):
     await message.answer_sticker('CAACAgIAAxkBAAMYZDKf_DzgwiVa2-OYSJx9FlGXQucAAlcYAAKG6ThKVoH7vhsaabIvBA')
     await message.answer(f'{message.from_user.first_name},Привет !!!!!',
                          reply_markup=main)
 
 @dp.message_handler(text='Понедельник')
-async def contacts(message: types.Message):
+async def contacts(message:types.Message):
     await message.answer(f'иди нахуй сука')
 
 @dp.message_handler(text='Вторник')
-async def contacts(message: types.Message):
+async def contacts(message:types.Message):
     await message.answer(f'я не ебурик')
 
 @dp.message_handler(text='Среда')
-async def contacts(message: types.Message):
+async def contacts(message:types.Message):
     await message.answer(f'эщкере')
 
 @dp.message_handler(text='Четверг')
-async def contacts(message: types.Message):
+async def contacts(message:types.Message):
     await message.answer(f'мама админа')
 
 @dp.message_handler(text='Пятница')
-async def contacts(message: types.Message):
+async def contacts(message:types.Message):
     await message.answer(f'хакбердин')
 
 @dp.message_handler(text='Суббота')
-async def contacts(message: types.Message):
+async def contacts(message:types.Message):
     await message.answer(f'каримов')
 
 @dp.message_handler(content_types=['sticker'])
@@ -50,7 +50,7 @@ async def check_sticker(message:types.Message):
     await bot.send_message(message.from_user.id, message.chat.id)
 
 @dp.message_handler(content_types=['document', 'photo'])
-async def forward_message(message: types.Message):
+async def forward_message(message:types.Message):
     await bot.forward_message(os.getenv('GROUP_ID'), message.from_user.id, message.message_id)
 
 
